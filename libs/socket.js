40cc44d8cc32fd6e7446097ddaf7c43d447e684f
Davide Missiato <davidemissiato00@gmail.com>
2022-04-07 21:45:05 +0200

    feature: deploy with GH pages

--- libs/socket.js
+++ libs/socket.js
@@ -1,7 +1,7 @@
 
 import io from 'socket.io-client'
 
-const socket = io.connect('http://localhost:8000');
+const socket = io.connect('https://damp-wildwood-91491.herokuapp.com/');
 
 export {
     socket
