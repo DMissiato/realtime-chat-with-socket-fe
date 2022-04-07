1849e1cdf6ab765a8c1fa9b6146b83c540a720c8
Davide Missiato <davidemissiato00@gmail.com>
2022-04-07 22:19:15 +0200

    fix: fix path and action

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/realtime-chat-with-socket-fe': { page: '/' },
+      '/realtime-chat-with-socket-fe/chat': { page: '/chat' },
+    }
+  }
 }
 
 module.exports = nextConfig
